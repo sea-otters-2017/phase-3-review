af3b6f66cdd997b3129eb97404bbef96f56eb2bb
Torey Hickman <torey@toreyhickman.com>
2016-07-11 10:33:35 -0400

    Updates default value for a post's hometown

--- part-2/app/models/post.rb
+++ part-2/app/models/post.rb
@@ -2,6 +2,6 @@ class Post < ActiveRecord::Base
   validates_presence_of :body, :guest_name
 
   def hometown=(town)
-    (!town.kind_of?(String) || town.empty?) ? super("unknown") : super(town)
+    (!town.kind_of?(String) || town.empty?) ? super("parts unknown") : super(town)
   end
 end
