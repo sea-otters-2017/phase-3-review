965a3834da1ae386a05143fe13a2712a0973b1c3
Eitan Greene <email": null>
2017-02-01 09:38:40 -0800

    Adds User model

--- part-3/blind-auction/app/models/user.rb
+++ part-3/blind-auction/app/models/user.rb
@@ -1,2 +1,25 @@
+require 'bcrypt'
+
 class User < ApplicationRecord
+  include 'bcrypt'
+
+  has_many :items
+
+  validates :username, presence: true, uniqueness: true
+  validates :email, :password_digest, presence: true
+
+  def password
+    @password ||= Password.new(password_digest)
+  end
+
+  def password=(new_password)
+    @password ||= Password.create(new_password)
+    self.password_digest = @password
+  end
+
+  def self.authenticate(username, password)
+    user = User.find_by(username: username)
+    return nil if user.nil? || user.password != password
+    user
+  end
 end
